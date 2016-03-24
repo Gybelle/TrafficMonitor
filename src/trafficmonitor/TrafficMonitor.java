7e5c572e4d39293b45a6f25f19957532df0b72eb
Gybelle <michelle.gybels@gmail.com>
2016-03-24 17:45:19 +0100

    Renamed main class. Ready to add some real code now!

---
+++ src/trafficmonitor/TrafficMonitor.java
@@ -0,0 +1,27 @@
+package trafficmonitor;
+
+import java.awt.*;
+import java.util.*;
+import javax.swing.*;
+
+/**
+ * Main class for Assignment 02: Build a traffic monitor tool for “your commute”
+ * using Google Maps.
+ * Authors: Michelle Gybels & Anaïs Ools
+ * Created on March 24, 2016
+ */
+
+public class TrafficMonitor {
+    private static void createAndShowGUI(){
+        //do something
+    }
+    
+    public static void main(String[] args){
+        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                createAndShowGUI();
+            }
+        });
+    }
+}
