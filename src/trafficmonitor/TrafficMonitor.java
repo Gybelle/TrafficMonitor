5accde95010b5575b42b1f534ae6a9d431e4ee50
Gybelle <michelle.gybels@gmail.com>
2016-03-24 18:21:40 +0100

    Created Frame. Guess these sizes are okay.

--- src/trafficmonitor/TrafficMonitor.java
+++ src/trafficmonitor/TrafficMonitor.java
@@ -1,22 +1,53 @@
 package trafficmonitor;
 
 import java.awt.*;
-import java.util.*;
 import javax.swing.*;
 
 /**
  * Main class for Assignment 02: Build a traffic monitor tool for “your commute”
  * using Google Maps.
- * Authors: Michelle Gybels & Anaïs Ools
+ * @author Michelle Gybels & Anaïs Ools
  * Created on March 24, 2016
  */
 
 public class TrafficMonitor {
-    private static void createAndShowGUI(){
-        //do something
+    //Constants
+    private static final int WINDOW_WIDTH = 1200;
+    private static final int WINDOW_HEIGHT = 700;
+
+    private final View mView;
+
+    public TrafficMonitor() {
+        mView = new View();
     }
-    
-    public static void main(String[] args){
+
+
+    /**
+     * Create the GUI for the application.
+     */
+    public void createGUI() {
+        JFrame frame = new JFrame("Traffic Monitor");
+        Dimension size = new Dimension(WINDOW_WIDTH, WINDOW_HEIGHT);
+        frame.setPreferredSize(size);
+        frame.setMinimumSize(size);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        //center the window on the screen
+        Dimension screen = Toolkit.getDefaultToolkit().getScreenSize();
+        frame.setLocation(screen.width / 2 - WINDOW_WIDTH / 2,
+                screen.height / 2 - WINDOW_HEIGHT / 2);
+
+        frame.add(mView);
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+    private static void createAndShowGUI() {
+        TrafficMonitor anothercoolapplication = new TrafficMonitor();
+        anothercoolapplication.createGUI();
+    }
+
+    public static void main(String[] args) {
         javax.swing.SwingUtilities.invokeLater(new Runnable() {
             @Override
             public void run() {
---
+++ src/trafficmonitor/View.java
@@ -0,0 +1,13 @@
+package trafficmonitor;
+
+import javax.swing.*;
+
+/**
+ * A view to represent the content of the application.
+ * @author Michelle Gybels and Anaïs Ools
+ */
+public class View extends JPanel {
+
+
+
+}
