5d88b1c732537698ec3b3abffb3340e6602e1cfc
Gybelle <michelle.gybels@gmail.com>
2016-03-25 14:29:23 +0100

    Added classes for the view.

---
+++ src/trafficmonitor/OutputView.java
@@ -0,0 +1,12 @@
+package trafficmonitor;
+
+import javax.swing.*;
+
+/**
+ * A view to represent the output part of the application.
+ * @author Michelle Gybels and Anaïs Ools
+ */
+
+public class OutputView extends JPanel {
+
+}
--- src/trafficmonitor/View.java
+++ src/trafficmonitor/View.java
@@ -6,6 +6,11 @@ import javax.swing.*;
  * A view to represent the content of the application.
  * @author Michelle Gybels and Anaïs Ools
  */
+
 public class View extends JPanel {
+    private InputView mInputView;
+    private OutputView mOuputView;
+
+
 
 }
